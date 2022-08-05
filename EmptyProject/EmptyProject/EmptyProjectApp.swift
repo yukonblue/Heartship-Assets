9a7b3bb1c1d0d605c5bbefbe1cf4e33f1d7a4f00
Yukon Blue <77284557+yukonblue@users.noreply.github.com>
2022-08-05 14:49:50 -0400

    Introduce `Assets/Assets.xcassets/Heart-Favorites.imageset` + Introduce `EmptyProject.xcodeproj`

---
+++ EmptyProject/EmptyProject/EmptyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyProjectApp.swift
+//  EmptyProject
+//
+//  Created by Z on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmptyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
